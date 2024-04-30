11367bc36a8e17de7ab7235fcf7a3599231e0369
Johann Mosin <89855069+AlataO@users.noreply.github.com>
2024-05-01 00:46:17 +0300

    Add files via upload

---
+++ Kinopoisk Redirect-1.0.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Kinopoisk Redirect
+// @namespace    https://t.me/johannmosin
+// @version      1.0
+// @description  Show a button on the page to redirect from kinopoisk.ru to C_X
+// @author       Johann Mosin
+// @match        https://www.kinopoisk.ru/*/*
+// @license      MIT
+// @grant        GM_addStyle
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @downloadURL https://github.com/AlataO/KinopoiskRedirect/blob/main/Kinopoisk%20Redirect.user.js
+// @updateURL https://github.com/AlataO/KinopoiskRedirect/blob/main/Kinopoisk%20Redirect.meta.js
+// ==/UserScript==
